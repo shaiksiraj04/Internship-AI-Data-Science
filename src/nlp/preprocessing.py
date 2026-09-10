2686f06400352df1766ea3a5056396b5fb473b22
shaiksiraj04 <jarismohammed7@gmail.com>
2026-09-10 23:57:53 +0530

    Updatingg,...

--- src/nlp/preprocessing.py
+++ src/nlp/preprocessing.py
@@ -1,21 +1,48 @@
-import nltk
 import string
-from nltk.tokenize import word_tokenize
+
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+
+
+STOP_WORDS = set(stopwords.words("english"))
+LEMMATIZER = WordNetLemmatizer()
+
+
+def get_wordnet_pos(tag):
+    if tag.startswith("J"):
+        return "a"
+    elif tag.startswith("V"):
+        return "v"
+    elif tag.startswith("N"):
+        return "n"
+    elif tag.startswith("R"):
+        return "r"
+    else:
+        return "n"
+
 
 def preprocess_text(text):
     text = text.lower()
+
     tokens = word_tokenize(text)
 
-    tokens = [word for word in tokens if word not in string.punctuation]
+    tokens = [
+        word for word in tokens
+        if word not in string.punctuation
+    ]
 
-    stop_words = set(stopwords.words('english'))
-    tokens = [word for word in tokens if word not in stop_words]
+    tokens = [
+        word for word in tokens
+        if word not in STOP_WORDS
+    ]
 
-    lemmatizer = WordNetLemmatizer()
-    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    tagged_tokens = pos_tag(tokens)
+
+    tokens = [
+        LEMMATIZER.lemmatize(word, get_wordnet_pos(tag))
+        for word, tag in tagged_tokens
+    ]
 
     return tokens
-
-print(preprocess_text("HELLO!!! I WANT TO REGISTER!!!"))
