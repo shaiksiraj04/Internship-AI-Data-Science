040dd7b7ad7e933b3da6dbfcaaaa1fe0e818ff34
shaiksiraj04 <jarismohammed7@gmail.com>
2026-09-10 23:49:03 +0530

    updatingg,...

--- src/nlp/preprocessing.py
+++ src/nlp/preprocessing.py
@@ -1,12 +1,20 @@
 import nltk
 import string
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 def preprocess_text(text):
     text = text.lower()
     tokens = word_tokenize(text)
 
     tokens = [word for word in tokens if word not in string.punctuation]
+
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words]
+
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
 
     return tokens
 
